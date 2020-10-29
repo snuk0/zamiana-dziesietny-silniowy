f05231d64697f3306bbbf7c415357b4ee02fc2f1
snuczek <26097711+snuk0@users.noreply.github.com>
2020-10-29 14:34:56 +0100

    Add files via upload

---
+++ 4.4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string silnia(int x) {
+	int silnia = 1;
+	int k = 1;
+	string s = "";
+	while (silnia < x) {
+		k = k + 1;
+		silnia = silnia * k;
+	}
+
+	if (silnia > x) {
+		silnia = silnia / k;
+		k = k - 1;
+	}
+	s = "";
+	while (k > 0) {
+		int cyfra = x/silnia;
+		s = s + char(cyfra+'0');
+		x = x - cyfra * silnia;
+		silnia = silnia / k;
+		k = k - 1;
+	}
+
+	return s;
+}
+
+int main() {
+	cout << silnia(5489);
+}
